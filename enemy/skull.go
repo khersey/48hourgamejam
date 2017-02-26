d854e3edbd56fdacf2d35ed7cc99e6f0046a016b
Kyle Hersey <khersey@khersey.local>
2017-02-26 16:44:15 -0500

    adds spooky ghost, quit and restart

--- enemy/skull.go
+++ enemy/skull.go
@@ -1,3 +1,18 @@
+components {
+  id: "script"
+  component: "/enemy/skull.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
@@ -12,8 +27,8 @@ embedded_components {
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
-  "      x: -0.70532244\n"
-  "      y: -9.521853\n"
+  "      x: -1.3006713\n"
+  "      y: -13.783486\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -25,8 +40,8 @@ embedded_components {
   "    index: 0\n"
   "    count: 3\n"
   "  }\n"
-  "  data: 11.410645\n"
-  "  data: 10.0\n"
+  "  data: 15.348836\n"
+  "  data: 6.2030945\n"
   "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
@@ -59,8 +74,8 @@ embedded_components {
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
-  "      x: -0.35266122\n"
-  "      y: 5.6425796\n"
+  "      x: -0.7991729\n"
+  "      y: 4.451882\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -72,8 +87,55 @@ embedded_components {
   "    index: 0\n"
   "    count: 3\n"
   "  }\n"
+  "  data: 20.7696\n"
+  "  data: 4.704058\n"
   "  data: 10.0\n"
-  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "platform"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"geometry\"\n"
+  "mask: \"hero\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: -1.3213193\n"
+  "      y: -5.368804\n"
+  "      z: 0.1\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 26.446743\n"
+  "  data: 3.0592978\n"
   "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
---
+++ enemy/spooky_ghost.go
@@ -0,0 +1,82 @@
+components {
+  id: "script"
+  component: "/enemy/spooky_ghost.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collisionobject"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"danger\"\n"
+  "mask: \"hero\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: -2.4186046\n"
+  "      y: -0.74418604\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 7.2093024\n"
+  "  data: 21.348837\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/def-runner/spooky_ghost/wizard_alive.tilesource\"\n"
+  "default_animation: \"alive\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
